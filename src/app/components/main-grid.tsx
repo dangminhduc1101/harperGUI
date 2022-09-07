dccde38acceeb6eb249512b59ab6725d624e1f5c
Duc <dangminhduc1101@gmail.com>
2022-09-07 17:25:17 -0400

    fixed grid and divider

--- src/app/components/main-grid.tsx
+++ src/app/components/main-grid.tsx
@@ -1,31 +1,29 @@
-import { Grid, Typography, Divider } from "@mui/material";
+import { Grid, Typography, Divider, Stack } from "@mui/material";
 import React from "react";
 import LeftDialogPanel from "./left-dialog";
 import RightDialogPanel from "./right-dialog";
 
 export default function MainGrid() {
   return (
-    <Grid container spacing={2}>
-      <Grid item xl={4} lg={4} md={12}>
+    <Grid container spacing={1}>
+      <Grid item md={12} lg={4} xl={4}>
         <Typography variant="h4" align="center">
           Left Controls
         </Typography>
         <LeftDialogPanel />
       </Grid>
-      <Divider orientation="vertical" flexItem>
-      </Divider>
-      <Grid item xl={4} lg={4} md={12}>
+        <Divider orientation="vertical" flexItem sx={{mr: "-1px"}}/>
+      <Grid item md={12} lg={4} xl={4}>
         <Typography variant="h4" align="center">
           Perception
         </Typography>
       </Grid>
-      <Divider orientation="vertical" flexItem>
-      </Divider>
-      <Grid item xl={4} lg={4} md={12}>
-        <Typography variant="h4" align="center">
-          Right Controls
-        </Typography>
-        <RightDialogPanel />
+        <Divider orientation="vertical" flexItem sx={{mr: "-1px"}}/>
+      <Grid item md={12} lg={4} xl={4}>
+          <Typography variant="h4" align="center">
+            Right Controls
+          </Typography>
+          <RightDialogPanel />
       </Grid>
     </Grid>
   );
